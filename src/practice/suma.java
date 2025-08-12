fa87706fa4653db2b52c0c0330c6105eb4cbf8b9
ErickCraft <moyae76@gmail.com>
2025-08-11 23:52:33 -0500

    Primer commit con la clase suma

---
+++ src/practice/suma.java
@@ -0,0 +1,16 @@
+package practice;
+
+
+public class suma {
+	public static void main(String[] arg) {
+		
+		sumar(6,7);
+		System.out.println(sumar(6, 7));
+	}
+
+	static int sumar(int a, int b) {
+		// TODO Auto-generated method stub
+		int result = a + b;
+		return result;
+	}
+}
