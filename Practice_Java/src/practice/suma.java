2f0dbd700b875186c284d30801328be0533930ce
ErickCraft <moyae76@gmail.com>
2025-08-12 00:25:15 -0500

    New commit

---
+++ Practice_Java/src/practice/suma.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class suma {
+public static void main(String[] arg) {
+	System.out.println(sumar(6,7));
+}
+
+static int sumar(int a, int b) {
+	int result = a + b;
+	return result;
+}
+}
